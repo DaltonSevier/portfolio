dbf265d7985914bfb81df0753213353c74885600
Dalton Sevier <daltonsevier@gmail.com>
2024-05-22 11:18:53 -0400

    implement recent projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -10,17 +12,12 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         
       <FloatingNav 
-          navItems={[
-            {
-              name: "Home",
-              link: "/",
-              icon: <FaHome />,
-            },
-          ]}
+          navItems={navItems}
           className="top-10"
       />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
